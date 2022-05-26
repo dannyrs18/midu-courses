42072e18deb476926d53ed3039b9b051cae38e56
Danny Romero <dannyors18@gmail.com>
2022-05-25 22:22:08 -0500

    Completly app anecdotes

--- Javascript y React Bootcamp/part1-react/anecdotes/src/App.js
+++ Javascript y React Bootcamp/part1-react/anecdotes/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = (props) => {
+
+  const [selected, setSelected] = useState(0);
+  const [voted, setVoted] = useState([]);
+
+  const nextAnecdote = () => {
+    if ( (selected + 1) === props.anecdotes.length )
+      return setSelected(0);
+    return setSelected(selected + 1);
+  }
+
+  const anecdoteVoted = (index) => {
+    const points = [...voted];
+    points[index] = points[index] ? points[index] + 1 : 1;
+    setVoted(points);
+  }
+
+  const totalVotes = voted.reduce((prev, curr) => prev + (curr || 0), 0);
+  let indexMostVotedAnecdote;
+  voted.forEach( (totalVotesAnecdote, index) => {
+    if (!indexMostVotedAnecdote && totalVotesAnecdote) indexMostVotedAnecdote = index;
+    indexMostVotedAnecdote = totalVotesAnecdote > voted[indexMostVotedAnecdote] ? index : indexMostVotedAnecdote 
+  });
+
+  console.log(indexMostVotedAnecdote)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        {props.anecdotes[selected]}
+      </div>
+      <div>
+        has {totalVotes} votes
+      </div>
+      <button onClick={nextAnecdote}>next anecdote</button>
+      <button onClick={() => anecdoteVoted(selected)}>vote</button>
+      <h2>Anecdote with the most votes</h2>
+      <div>
+        {props.anecdotes[indexMostVotedAnecdote]}
+      </div>
+      <div>
+        has {voted[indexMostVotedAnecdote]}
+      </div>
     </div>
-  );
+  )
 }
+
 
 export default App;
